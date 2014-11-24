dff7fe2cdfb64c1f6a4a2baf105b980d155b695f
umzk <dedo99999@gmail.com>
2014-11-24 18:28:58 +0600

    ooopc fix

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -2,5 +2,6 @@ class Apartment < ActiveRecord::Base
 
   validates :meter, :room, :floor, numericality: { greater_than: 0}
   validates :room, :address,  presence: true
-  validates :meter, :room, :floor, :address, :area, :type, length: { maximum: 300 }}
+  validates :meter, :room, :floor, :address, :area, :type, length: { maximum: 300 }
+
 end
