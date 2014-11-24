abce314d8a7465b69f2380ea1b8c8f06db48a2bd
umzk <dedo99999@gmail.com>
2014-11-24 17:41:08 +0600

    add validations

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,6 @@
 class Apartment < ActiveRecord::Base
+
+  validates :meter, :room, :floor, numericality: { greater_than: 0}
+  validates :room, :address,  presence: true
+  validates :meter, :room, :floor, :address, :area, :type, length: { maximum: 300 }}
 end
